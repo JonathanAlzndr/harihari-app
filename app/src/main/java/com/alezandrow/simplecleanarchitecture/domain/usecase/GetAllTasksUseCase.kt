c564891c8e9baaf005358e2066e94e7ccc49d100
AlezandRow <jkamagi41@gmail.com>
2026-06-18 22:58:05 +0800

    fix(domain): change the suspend function into normal function on GetAllTasksUseCase

--- app/src/main/java/com/alezandrow/simplecleanarchitecture/domain/usecase/GetAllTasksUseCase.kt
+++ app/src/main/java/com/alezandrow/simplecleanarchitecture/domain/usecase/GetAllTasksUseCase.kt
@@ -3,5 +3,5 @@ package com.alezandrow.simplecleanarchitecture.domain.usecase
 import com.alezandrow.simplecleanarchitecture.domain.repository.ITaskRepository
 
 class GetAllTasksUseCase(private val repository: ITaskRepository) {
-    suspend operator fun invoke() = repository.getAllTasks()
+   operator fun invoke() = repository.getAllTasks()
 }
