2f0515b731ef215deeddbce070d91ae64e3c43ec
AlezandRow <jkamagi41@gmail.com>
2026-06-19 14:03:15 +0800

    fix(domain): add default value for task id

--- app/src/main/java/com/alezandrow/simplecleanarchitecture/domain/entities/Task.kt
+++ app/src/main/java/com/alezandrow/simplecleanarchitecture/domain/entities/Task.kt
@@ -1,7 +1,7 @@
 package com.alezandrow.simplecleanarchitecture.domain.entities
 
 data class Task(
-    val id: Int,
+    val id: Int = 0,
     val description: String,
     val status: TaskStatus
 )
