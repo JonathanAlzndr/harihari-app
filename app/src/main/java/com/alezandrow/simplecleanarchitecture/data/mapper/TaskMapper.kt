0ab10c8142e9439def25fa6e8887053724c87d1c
AlezandRow <jkamagi41@gmail.com>
2026-06-19 14:04:10 +0800

    fix(data): add id field to mapper toTaskDbEntity

--- app/src/main/java/com/alezandrow/simplecleanarchitecture/data/mapper/TaskMapper.kt
+++ app/src/main/java/com/alezandrow/simplecleanarchitecture/data/mapper/TaskMapper.kt
@@ -18,6 +18,7 @@ fun TaskDbEntity.toDomain(): Task {
 
 fun Task.toDbEntity(): TaskDbEntity {
     return TaskDbEntity(
+        id = id,
         description = description,
         status = status.name
     )
