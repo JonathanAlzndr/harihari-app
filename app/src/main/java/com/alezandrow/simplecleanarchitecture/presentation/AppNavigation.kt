9fc75db5a5a05c25ed27cb25be78733a83897538
AlezandRow <jkamagi41@gmail.com>
2026-06-19 15:01:11 +0800

    feat(presentation): create AppNavigation composable

---
+++ app/src/main/java/com/alezandrow/simplecleanarchitecture/presentation/AppNavigation.kt
@@ -0,0 +1,43 @@
+package com.alezandrow.simplecleanarchitecture.presentation
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
+import com.alezandrow.simplecleanarchitecture.presentation.components.AddTaskDialog
+import com.alezandrow.simplecleanarchitecture.presentation.screen.HomeScreen
+import com.alezandrow.simplecleanarchitecture.presentation.screen.HomeViewModel
+
+@Composable
+fun AppNavigation(modifier: Modifier = Modifier, viewModel: HomeViewModel = hiltViewModel()) {
+
+    var showDialog by remember { mutableStateOf(false) }
+
+    Scaffold(
+        modifier,
+        floatingActionButton = {
+            FloatingActionButton(onClick = { showDialog = true }) {
+                Icon(
+                    imageVector = Icons.Default.Add,
+                    contentDescription = "Add Task"
+                )
+            }
+        }
+    ) { innerPadding ->
+        HomeScreen(Modifier.padding(innerPadding), viewModel)
+
+        if (showDialog) {
+            AddTaskDialog(onConfirmation = viewModel::addNewTask, { showDialog = false })
+        }
+
+    }
+}
