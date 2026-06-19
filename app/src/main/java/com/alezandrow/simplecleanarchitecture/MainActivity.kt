9e4b5fef225941886caddda7874389696a209731
AlezandRow <jkamagi41@gmail.com>
2026-06-19 16:49:46 +0800

    feat(presentation): call AppNavigation from MainActivity

--- app/src/main/java/com/alezandrow/simplecleanarchitecture/MainActivity.kt
+++ app/src/main/java/com/alezandrow/simplecleanarchitecture/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import com.alezandrow.simplecleanarchitecture.presentation.AppNavigation
 import com.alezandrow.simplecleanarchitecture.presentation.theme.SimpleCleanArchitectureTheme
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -14,7 +15,7 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             SimpleCleanArchitectureTheme {
-
+                AppNavigation()
             }
         }
     }
