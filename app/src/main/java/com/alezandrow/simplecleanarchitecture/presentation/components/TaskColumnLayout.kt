0ff11599655375372e05b0bab0d8480270177e17
AlezandRow <jkamagi41@gmail.com>
2026-06-19 14:53:29 +0800

    feat(presentation): update the TaskColumnLayout component

---
+++ app/src/main/java/com/alezandrow/simplecleanarchitecture/presentation/components/TaskColumnLayout.kt
@@ -0,0 +1,51 @@
+package com.alezandrow.simplecleanarchitecture.presentation.components
+
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material3.SwipeToDismissBox
+import androidx.compose.material3.SwipeToDismissBoxValue
+import androidx.compose.material3.rememberSwipeToDismissBoxState
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.alezandrow.simplecleanarchitecture.domain.entities.Task
+
+@Composable
+fun TaskColumnLayout(
+    tasks: List<Task>,
+    onClickAction: (Task) -> Unit,
+    onDeleteTask: (Task) -> Unit,
+    modifier: Modifier = Modifier
+) {
+    LazyColumn(
+        modifier,
+        contentPadding = PaddingValues(16.dp),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+
+        items(items = tasks, key = { it.id }) { task ->
+
+            val dismissState = rememberSwipeToDismissBoxState(
+                confirmValueChange = { value ->
+                    if(value == SwipeToDismissBoxValue.EndToStart) {
+                        onDeleteTask(task)
+                        true
+                    } else {
+                        false
+                    }
+                }
+            )
+
+            SwipeToDismissBox(
+                state = dismissState,
+                backgroundContent = { SwipeBackground(dismissState) },
+                enableDismissFromStartToEnd = false,
+            ) {
+                TaskCard(task, onClickAction)
+            }
+        }
+
+    }
+}
